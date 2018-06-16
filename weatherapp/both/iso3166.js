772540fbfad9791939b09a74e49c669709946ff2
Dirk Stolle <striezel-dev@web.de>
2018-06-16 20:47:05 +0200

    better formatting for ISO 3166 code lookup

--- weatherapp/both/iso3166.js
+++ weatherapp/both/iso3166.js
@@ -1,91 +1,363 @@
 /* A (yet very incomplete) list of country codes according to ISO 3166. */
 codesISO3166 = {
-  'AD': {de: 'Andorra', en: 'Andorra'},
-  'AF': {de: 'Afghanistan', en: 'Afghanistan'},
-  'AG': {de: 'Antigua und Barbuda', en: 'Antigua and Barbuda'},
-  'AI': {de: 'Anguilla', en: 'Anguilla'},
-  'AL': {de: 'Albanien', en: 'Albania'},
-  'AM': {de: 'Armenien', en: 'Armenia'},
-  'AO': {de: 'Angola', en: 'Angola'},
-  'AQ': {de: 'Antarktika', en: 'Antarctica'},
-  'AR': {de: 'Argentinien', en: 'Argentina'},
-  'AS': {de: 'Amerikanisch-Samoa', en: 'American Samoa'},
-  'AU': {de: 'Australien', en: 'Australia'},
-  'AW': {de: 'Aruba', en: 'Aruba'},
-  'AX': {de: 'Ålandinseln', en: 'Åland'},
-  'AZ': {de: 'Aserbaidschan', en: 'Azerbaijan'},
-  'BE': {de: 'Belgien', en: 'Belgium'},
-  'BG': {de: 'Bulgarien', en: 'Bulgaria'},
-  'BH': {de: 'Bahrain', en: 'Bahrain'},
-  'BO': {de: 'Bolivien', en: 'Bolivia'},
-  'BS': {de: 'Bahamas', en: 'Bahamas'},
-  'BR': {de: 'Brasilien', en: 'Brazil'},
-  'BY': {de: 'Weißrussland', en: 'Belarus'},
-  'CA': {de: 'Kanada', en: 'Canada'},
-  'CI': {de: 'Elfenbeinküste', en: 'Ivory Coast'},
-  'CL': {de: 'Chile', en: 'Chile'},
-  'CN': {de: 'China', en: 'China'},
-  'CR': {de: 'Costa Rica', en: 'Costa Rica'},
-  'CU': {de: 'Kuba', en: 'Cuba'},
-  'CW': {de: 'Curaçao', en: 'Curaçao'},
-  'DE': {de: 'Deutschland', en: 'Germany'},
-  'DK': {de: 'Dänemark', en: 'Denmark'},
-  'DZ': {de: 'Algerien', en: 'Algeria'},
-  'EC': {de: 'Ecuador', en: 'Ecuador'},
-  'EE': {de: 'Estland', en: 'Estonia'},
-  'EG': {de: 'Ägypten', en: 'Egypt'},
-  'ES': {de: 'Spanien', en: 'Spain'},
-  'FI': {de: 'Finnland', en: 'Finland'},
-  'FR': {de: 'Frankreich', en: 'France'},
-  'GB': {de: 'Vereinigtes Königreich', en: 'Great Britain'},
-  'GR': {de: 'Griechenland', en: 'Greece'},
-  'HK': {de: 'Hongkong', en: 'Hongkong'},
-  'HN': {de: 'Honduras', en: 'Honduras'},
-  'HT': {de: 'Haiti', en: 'Haiti'},
-  'HU': {de: 'Ungarn', en: 'Hungary'},
-  'ID': {de: 'Indonesien', en: 'Indonesia'},
-  'HK': {de: 'Hongkong', en: 'Hong Kong'},
-  'HR': {de: 'Kroatien', en: 'Croatia'},
-  'HT': {de: 'Haiti', en: 'Haiti'},
-  'IE': {de: 'Irland', en: 'Ireland'},
-  'IL': {de: 'Israel', en: 'Israel'},
-  'IN': {de: 'Indien', en: 'India'},
-  'IS': {de: 'Island', en: 'Iceland'},
-  'IT': {de: 'Italien', en: 'Italy'},
-  'JE': {de: 'Jersey', en: 'Jersey'},
-  'JM': {de: 'Jamaika', en: 'Jamaica'},
-  'JO': {de: 'Jordanien', en: 'Jordan'},
-  'JP': {de: 'Japan', en: 'Japan'},
-  'KE': {de: 'Kenia', en: 'Kenya'},
-  'KI': {de: 'Kiribati', en: 'Kiribati'},
-  'KZ': {de: 'Kasachstan', en: 'Kazakhstan'},
-  'LV': {de: 'Lettland', en: 'Latvia'},
-  'MC': {de: 'Monaco', en: 'Monaco'},
-  'MN': {de: 'Mongolei', en: 'Mongolia'},
-  'MO': {de: 'Macau', en: 'Macao'},
-  'MT': {de: 'Malta', en: 'Malta'},
-  'MV': {de: 'Malediven', en: 'Maldives'},
-  'MX': {de: 'Mexiko', en: 'Mexico'},
-  'NL': {de: 'Niederlande', en: 'Netherlands'},
-  'NO': {de: 'Norwegen', en: 'Norway'},
-  'NZ': {de: 'Neuseeland', en: 'New Zealand'},
-  'PH': {de: 'Philippinen', en: 'Philippines'},
-  'PL': {de: 'Polen', en: 'Poland'},
-  'PR': {de: 'Puerto Rico', en: 'Puerto Rico'},
-  'PT': {de: 'Portugal', en: 'Portugal'},
-  'RE': {de: 'Reunion', en: 'Reunion'},
-  'RO': {de: 'Rumänien', en: 'Romania'},
-  'RU': {de: 'Russische Föderation', en: 'Russian Federation'},
-  'RW': {de: 'Ruanda', en: 'Rwanda'},
-  'SA': {de: 'Saudi-Arabien', en: 'Saudi Arabia'},
-  'SI': {de: 'Slowenien', en: 'Slovenia'},
-  'SL': {de: 'Slowakei', en: 'Slovakia'},
-  'SN': {de: 'Senegal', en: 'Senegal'},
-  'SR': {de: 'Suriname', en: 'Suriname'},
-  'SV': {de: 'El Salvador', en: 'El Salvador'},
-  'SY': {de: 'Syrien', en: 'Syria'},
-  'SZ': {de: 'Swasiland', en: 'Swaziland'},
-  'TR': {de: 'Türkei', en: 'Turkey'},
-  'US': {de: 'Vereinigte Staaten von Amerika', en: 'United States of America'},
-  'VI': {de: 'Amerikanische Jungferninseln', en: 'Virgin Islands (U.S.)'}
+  'AD': {
+    de: 'Andorra',
+    en: 'Andorra'
+  },
+  'AF': {
+    de: 'Afghanistan',
+    en: 'Afghanistan'
+  },
+  'AG': {
+    de: 'Antigua und Barbuda',
+    en: 'Antigua and Barbuda'
+  },
+  'AI': {
+    de: 'Anguilla',
+    en: 'Anguilla'
+  },
+  'AL': {
+    de: 'Albanien',
+    en: 'Albania'
+  },
+  'AM': {
+    de: 'Armenien',
+    en: 'Armenia'
+  },
+  'AO': {
+    de: 'Angola',
+    en: 'Angola'
+  },
+  'AQ': {
+    de: 'Antarktika',
+    en: 'Antarctica'
+  },
+  'AR': {
+    de: 'Argentinien',
+    en: 'Argentina'
+  },
+  'AS': {
+    de: 'Amerikanisch-Samoa',
+    en: 'American Samoa'
+  },
+  'AU': {
+    de: 'Australien',
+    en: 'Australia'
+  },
+  'AW': {
+    de: 'Aruba',
+    en: 'Aruba'
+  },
+  'AX': {
+    de: 'Ålandinseln',
+    en: 'Åland'
+  },
+  'AZ': {
+    de: 'Aserbaidschan',
+    en: 'Azerbaijan'
+  },
+  'BE': {
+    de: 'Belgien',
+    en: 'Belgium'
+  },
+  'BG': {
+    de: 'Bulgarien',
+    en: 'Bulgaria'
+  },
+  'BH': {
+    de: 'Bahrain',
+    en: 'Bahrain'
+  },
+  'BO': {
+    de: 'Bolivien',
+    en: 'Bolivia'
+  },
+  'BS': {
+    de: 'Bahamas',
+    en: 'Bahamas'
+  },
+  'BR': {
+    de: 'Brasilien',
+    en: 'Brazil'
+  },
+  'BY': {
+    de: 'Weißrussland',
+    en: 'Belarus'
+  },
+  'CA': {
+    de: 'Kanada',
+    en: 'Canada'
+  },
+  'CI': {
+    de: 'Elfenbeinküste',
+    en: 'Ivory Coast'
+  },
+  'CL': {
+    de: 'Chile',
+    en: 'Chile'
+  },
+  'CN': {
+    de: 'China',
+    en: 'China'
+  },
+  'CR': {
+    de: 'Costa Rica',
+    en: 'Costa Rica'
+  },
+  'CU': {
+    de: 'Kuba',
+    en: 'Cuba'
+  },
+  'CW': {
+    de: 'Curaçao',
+    en: 'Curaçao'
+  },
+  'DE': {
+    de: 'Deutschland',
+    en: 'Germany'
+  },
+  'DK': {
+    de: 'Dänemark',
+    en: 'Denmark'
+  },
+  'DZ': {
+    de: 'Algerien',
+    en: 'Algeria'
+  },
+  'EC': {
+    de: 'Ecuador',
+    en: 'Ecuador'
+  },
+  'EE': {
+    de: 'Estland',
+    en: 'Estonia'
+  },
+  'EG': {
+    de: 'Ägypten',
+    en: 'Egypt'
+  },
+  'ES': {
+    de: 'Spanien',
+    en: 'Spain'
+  },
+  'FI': {
+    de: 'Finnland',
+    en: 'Finland'
+  },
+  'FR': {
+    de: 'Frankreich',
+    en: 'France'
+  },
+  'GB': {
+    de: 'Vereinigtes Königreich',
+    en: 'Great Britain'
+  },
+  'GR': {
+    de: 'Griechenland',
+    en: 'Greece'
+  },
+  'HK': {
+    de: 'Hongkong',
+    en: 'Hongkong'
+  },
+  'HN': {
+    de: 'Honduras',
+    en: 'Honduras'
+  },
+  'HT': {
+    de: 'Haiti',
+    en: 'Haiti'
+  },
+  'HU': {
+    de: 'Ungarn',
+    en: 'Hungary'
+  },
+  'ID': {
+    de: 'Indonesien',
+    en: 'Indonesia'
+  },
+  'HK': {
+    de: 'Hongkong',
+    en: 'Hong Kong'
+  },
+  'HR': {
+    de: 'Kroatien',
+    en: 'Croatia'
+  },
+  'HT': {
+    de: 'Haiti',
+    en: 'Haiti'
+  },
+  'IE': {
+    de: 'Irland',
+    en: 'Ireland'
+  },
+  'IL': {
+    de: 'Israel',
+    en: 'Israel'
+  },
+  'IN': {
+    de: 'Indien',
+    en: 'India'
+  },
+  'IS': {
+    de: 'Island',
+    en: 'Iceland'
+  },
+  'IT': {
+    de: 'Italien',
+    en: 'Italy'
+  },
+  'JE': {
+    de: 'Jersey',
+    en: 'Jersey'
+  },
+  'JM': {
+    de: 'Jamaika',
+    en: 'Jamaica'
+  },
+  'JO': {
+    de: 'Jordanien',
+    en: 'Jordan'
+  },
+  'JP': {
+    de: 'Japan',
+    en: 'Japan'
+  },
+  'KE': {
+    de: 'Kenia',
+    en: 'Kenya'
+  },
+  'KI': {
+    de: 'Kiribati',
+    en: 'Kiribati'
+  },
+  'KZ': {
+    de: 'Kasachstan',
+    en: 'Kazakhstan'
+  },
+  'LV': {
+    de: 'Lettland',
+    en: 'Latvia'
+  },
+  'MC': {
+    de: 'Monaco',
+    en: 'Monaco'
+  },
+  'MN': {
+    de: 'Mongolei',
+    en: 'Mongolia'
+  },
+  'MO': {
+    de: 'Macau',
+    en: 'Macao'
+  },
+  'MT': {
+    de: 'Malta',
+    en: 'Malta'
+  },
+  'MV': {
+    de: 'Malediven',
+    en: 'Maldives'
+  },
+  'MX': {
+    de: 'Mexiko',
+    en: 'Mexico'
+  },
+  'NL': {
+    de: 'Niederlande',
+    en: 'Netherlands'
+  },
+  'NO': {
+    de: 'Norwegen',
+    en: 'Norway'
+  },
+  'NZ': {
+    de: 'Neuseeland',
+    en: 'New Zealand'
+  },
+  'PH': {
+    de: 'Philippinen',
+    en: 'Philippines'
+  },
+  'PL': {
+    de: 'Polen',
+    en: 'Poland'
+  },
+  'PR': {
+    de: 'Puerto Rico',
+    en: 'Puerto Rico'
+  },
+  'PT': {
+    de: 'Portugal',
+    en: 'Portugal'
+  },
+  'RE': {
+    de: 'Reunion',
+    en: 'Reunion'
+  },
+  'RO': {
+    de: 'Rumänien',
+    en: 'Romania'
+  },
+  'RU': {
+    de: 'Russische Föderation',
+    en: 'Russian Federation'
+  },
+  'RW': {
+    de: 'Ruanda',
+    en: 'Rwanda'
+  },
+  'SA': {
+    de: 'Saudi-Arabien',
+    en: 'Saudi Arabia'
+  },
+  'SI': {
+    de: 'Slowenien',
+    en: 'Slovenia'
+  },
+  'SL': {
+    de: 'Slowakei',
+    en: 'Slovakia'
+  },
+  'SN': {
+    de: 'Senegal',
+    en: 'Senegal'
+  },
+  'SR': {
+    de: 'Suriname',
+    en: 'Suriname'
+  },
+  'SV': {
+    de: 'El Salvador',
+    en: 'El Salvador'
+  },
+  'SY': {
+    de: 'Syrien',
+    en: 'Syria'
+  },
+  'SZ': {
+    de: 'Swasiland',
+    en: 'Swaziland'
+  },
+  'TR': {
+    de: 'Türkei',
+    en: 'Turkey'
+  },
+  'US': {
+    de: 'Vereinigte Staaten von Amerika',
+    en: 'United States of America'
+  },
+  'UY': {
+    de: 'Uruguay',
+    en: 'Uruguay'
+  },
+  'VI': {
+    de: 'Amerikanische Jungferninseln',
+    en: 'Virgin Islands (U.S.)'
+  },
+  'ZA': {
+    de: 'Südafrika',
+    en: 'South Africa'
+  }
 };
